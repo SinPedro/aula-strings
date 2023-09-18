9a3c82505c9a5b85b7f3c1770890bbe4340061b7
Pedro de Souza Martins <143435476+SinPedro@users.noreply.github.com>
2023-09-18 14:00:33 -0300

    Add files via upload

---
+++ comidaspreferidas/comidas.js
@@ -0,0 +1,38 @@
+//let array
+//console.log('a. ', array)
+//array = null
+//console.log('b. ', array)
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
+//let i = 0
+//console.log('d. ', array[i])
+//array[i+1] = 19
+//console.log('e. ', array)
+//const valor = array[i+6]
+//console.log('f. ', valor)
+const comidasPreferidas = ["alaminuta","brócolis","strogonof","churrasco","carreteiro","bolo-cenoura"]
+console.log("Minhas comidas preferidas são:");
+console.log("0:", comidasPreferidas[0]);
+console.log("1:", comidasPreferidas[1]);
+console.log("2:", comidasPreferidas[2]);
+console.log("3:", comidasPreferidas[3]);
+console.log("4:", comidasPreferidas[4]);
+console.log("5:", comidasPreferidas[5]);
+
+
+const novaComida = prompt("Por favor me informe sua comida preferida:")
+comidasPreferidas[2] = novaComida
+
+console.log("Minhas e suas novas comidas preferidas são:");
+console.log("0:", comidasPreferidas[0]);
+console.log("1:", comidasPreferidas[1]);
+console.log("2:", comidasPreferidas[2]);
+console.log("3:", comidasPreferidas[3]);
+console.log("4:", comidasPreferidas[4]);
+console.log("5:", comidasPreferidas[5]);
+
+
+
+
+
+
